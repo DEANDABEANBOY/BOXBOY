43cf7318cabe41abd0e43efea259a375713cfcdd
DEANDABEANBOY <87534955+DEANDABEANBOY@users.noreply.github.com>
2021-07-16 10:20:14 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+  box=createSprite(200,200,50,50);
+}
+
+function draw() 
+{
+  background(30);
+
+  if(keyDown("RIGHT_ARROW")){
+    box.position.x=box.position.x+3;
+  }
+drawSprites();
+}
+
+
+
+
